88106f659d8860fa51bf97583c3b5459ee4df3f2
Linus Schlumberger <l@mll.one>
2022-04-26 12:47:45 +0200

    feat(frontend): improve images caching

--- frontend/src/pictures.ts
+++ frontend/src/pictures.ts
@@ -1,36 +1,50 @@
 import axios from 'axios';
+import { ReplaySubject } from 'rxjs';
 
-const previousPictures: Record<string, string> = {};
+const previousPictures: Record<string, ReplaySubject<string>> = {};
 
 const picturesKey = import.meta.env.VITE_FOOD_WASTE_PICTURES_KEY as
   | string
   | undefined;
 
-export const getPicture = async (search: string) => {
-  if (!picturesKey) {
-    return '';
-  }
-  if (previousPictures[search]) {
-    return previousPictures[search];
-  }
-  const result = await axios.get('https://api.pexels.com/v1/search', {
-    params: {
-      query: search,
-      size: 'medium',
-      per_page: 1,
-    },
-    headers: {
-      Authorization: picturesKey,
-    },
+export const getPicture = (search: string) =>
+  new Promise<string>((resolve, reject) => {
+    (async () => {
+      try {
+        if (!picturesKey) {
+          resolve('');
+          return;
+        }
+        if (previousPictures[search]) {
+          previousPictures[search].subscribe((item) => {
+            resolve(item);
+          });
+        } else {
+          previousPictures[search] = new ReplaySubject();
+          const result = await axios.get('https://api.pexels.com/v1/search', {
+            params: {
+              query: search,
+              size: 'medium',
+              per_page: 1,
+            },
+            headers: {
+              Authorization: picturesKey,
+            },
+          });
+          if (
+            result &&
+            result.data &&
+            result.data.photos &&
+            result.data.photos.length
+          ) {
+            previousPictures[search].next(result.data.photos[0].src.medium);
+            resolve(result.data.photos[0].src.medium);
+            return;
+          }
+          resolve('');
+        }
+      } catch (err) {
+        reject(err);
+      }
+    })();
   });
-  if (
-    result &&
-    result.data &&
-    result.data.photos &&
-    result.data.photos.length
-  ) {
-    previousPictures[search] = result.data.photos[0].src.medium;
-    return result.data.photos[0].src.medium;
-  }
-  return '';
-};
