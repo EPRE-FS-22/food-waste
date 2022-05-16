37ce30e88f860bbe0eebd2d41120d8db5f4beac7
Oliver <olivergrun.og@gmail.com>
2022-05-16 09:31:33 +0200

    feat: added WIP version of id checker

--- backend/src/validateId.ts
+++ backend/src/validateId.ts
@@ -1,6 +1,43 @@
+import tfn from '@tensorflow/tfjs-node';
+import {testImage} from "./testimage.js";
+import sharp from 'sharp'
+
 export const validateId = async (idBase64: string) => {
+  const CLASS_NAMES = ['nonid', 'nonswiss', 'swissid']
+  /**const buf = await resizeImg(fs.readFileSync('/Users/oliver/HSLU/EPRE/tensorflow-test/Test/20161207_142428_HDR.jpg'),{
+    width: 180,
+    height: 180
+  })**/
+    //const buf = await fs.readFileSync('/Users/oliver/HSLU/EPRE/tensorflow-test/Test/20161207_142428_HDR.jpg');
+  const imgBuffer = Buffer.from(idBase64);
+  const scaledBuffer = await sharp(imgBuffer)
+    .resize(180, 180)
+    .jpeg()
+    .toBuffer();
+
+  let img = tfn.node.decodeJpeg(scaledBuffer);
+  img = img.expandDims();
+  img = img.cast('float32');
+  console.log(img.toString())
+  const model = await tfn.node.loadSavedModel('./src/identifier');
+  const predictions = model.predict(img)
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const score = tfn.softmax(predictions);
+  console.log(score.toString())
   if (idBase64) {
     return true;
   }
   return false;
 };
+
+
+(async () => {
+  try {
+    await validateId(testImage);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+
+})();
